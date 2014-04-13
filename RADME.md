36942ec67db33810785b2e2c8879e10b9e535f06
Adam Hartwell <adamhartwell2@gmail.com>
2014-04-13 21:58:05 +0100

    Initial Commit

---
+++ RADME.md
@@ -0,0 +1,9 @@
+geneticAlgorithmTest
+=========
+
+A simple implementation of a genetic algorithm to find the maximum of a function, exercise taken from silde 36 of cs.uwindsor.ca/~angom/teaching/cs574/ch9.pdf
+
+Use
+========
+
+Built and tested on Python 2.7.6, run the script and viola.
---
+++ genetics.py
@@ -0,0 +1,19 @@
+'Functions to implement the genetic algorithm'
+import random
+
+'Create a member of the population'
+def individual():
+    
+
+'Generate a population - some number of individuals'
+def populate(count):
+    
+
+'Calculate the fitness of an individual'
+def fitness(individual):
+    
+
+'Take a population and evolve it one generation'
+def evolve(pop):
+    
+    
